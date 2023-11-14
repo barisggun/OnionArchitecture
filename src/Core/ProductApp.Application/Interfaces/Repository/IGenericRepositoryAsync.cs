8d1866a493cd930a0ededbbb0d75e0efca5ac13c
Barış Gün <108459412+bgun7@users.noreply.github.com>
2023-11-14 15:26:16 +0300

    add application layer

---
+++ src/Core/ProductApp.Application/Interfaces/Repository/IGenericRepositoryAsync.cs
@@ -0,0 +1,17 @@
+﻿using ProductApp.Domain.Common;
+using ProductApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.Application.Interfaces.Repository
+{
+    public interface IGenericRepositoryAsync<T> where T : BaseEntity //Base entity türünden türemiş bir class istiyoruz
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T> GetById(Guid Id);
+        Task<T> AddAsync(T entity);
+    }
+}
