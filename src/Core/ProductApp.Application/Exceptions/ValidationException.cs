dfeeeabfa8b61a9d383cfca4bd40b4382a80786c
Barış Gün <108459412+bgun7@users.noreply.github.com>
2023-11-14 14:59:16 +0300

    Add project files.

---
+++ src/Core/ProductApp.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.Application.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public ValidationException() : this("Validation error occured")
+        {
+            //hiçbir parametre gelmiyorsa böyle bir hata oluşsun
+            //message olarak bunu gönderecek 
+        }
+
+        public ValidationException(string Message) : base(Message)
+        {
+            //string mesaj geliyorsa biz onu base'e message olarak gönderelim
+        }
+
+        public ValidationException(Exception ex) : this(ex.Message)
+        {
+            
+        }
+    }
+}
